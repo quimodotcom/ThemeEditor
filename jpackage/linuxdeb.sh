e738308607254d55ae1727a808ed828c905f8d28
quimodotcom <evandonald01@gmail.com>
2021-08-25 17:21:07 +0100

    new changes

---
+++ jpackage/linuxdeb.sh
@@ -0,0 +1,1 @@
+jpackage -t deb -i /home/quimo/themeeditor/jpackage/input/ -d /home/quimo/themeeditor/jpackage/output -n ThemeEditor --icon /home/quimo/themeeditor/jpackage/icon/icon.icns --main-jar ThemeEditor-1.0A.jar --main-class Main --linux-shortcut
